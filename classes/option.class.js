ab441b548d568b4eae08764002a949e0f318a7be
Sebastian Wolff <sebastian.wolff@iu-study.org>
2023-11-29 10:31:59 +0100

    added mute and fulsscreen button to startscreen

--- classes/option.class.js
+++ classes/option.class.js
@@ -1,3 +1,13 @@
 class Option extends StatusBar {
-
+    height = 50;
+    width = 50;
+    isMuteBtn;
+    isFullscreenSwitch;
+    constructor(img, posX, posY, isMuteBtn, isFullscreenSwitch){
+        super().loadImage(img);
+        this.positionX = posX;
+        this.positionY = posY;
+        this.isMuteBtn = isMuteBtn;
+        this.isFullscreenSwitch = isFullscreenSwitch;
+    }
 }
--- classes/startScreen.class.js
+++ classes/startScreen.class.js
@@ -3,7 +3,9 @@ class StartScreen {
     canvas;
     startScreenImages = startImages;
     stopDrawing = false;
-    playBtn = new PlayButton(430, 460)
+    playBtn = new PlayButton(430, 260);
+    muteBtn = new Option('buttons/mute.png', 940, 20, true, false);
+    fullscreenBtn = new Option('buttons/fullscreen.png', 1000, 20, false, true)
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas
@@ -18,6 +20,8 @@ class StartScreen {
         this.addObjectsToMap(this.startScreenImages.potions);
         this.addObjectsToMap(this.startScreenImages.enemies);
         this.addToMap(this.playBtn);
+        this.addToMap(this.muteBtn);
+        this.addToMap(this.fullscreenBtn);
         let self = this;
         if (!this.stopDrawing) {
             requestAnimationFrame(() => {
@@ -36,6 +40,8 @@ class StartScreen {
     addToMap(obj) {
         obj.draw(this.ctx);
         this.startGameButtonListener(obj);
+        this.startMuteButtonListener(obj);
+        this.startFullscreenListener(obj);
     }
 
     startGameButtonListener(obj) {
@@ -44,16 +50,45 @@ class StartScreen {
                 const canvasRect = this.canvas.getBoundingClientRect();
                 const clickX = event.clientX - canvasRect.left;
                 const clickY = event.clientY - canvasRect.top;
-                if (
-                    clickX >= obj.positionX &&
-                    clickX <= obj.positionX + obj.width &&
-                    clickY >= obj.positionY &&
-                    clickY <= obj.positionY + obj.height
-                ) {
+                if (this.isClickingOnElement(clickX, clickY, obj)) {
                     this.stopDrawing = true;
                 }
             });
         }
+    }
+
+    startMuteButtonListener(obj) {
+        if (obj instanceof Option && obj.isMuteBtn) {
+            this.canvas.addEventListener('click', (event) => {
+                const canvasRect = this.canvas.getBoundingClientRect();
+                const clickX = event.clientX - canvasRect.left;
+                const clickY = event.clientY - canvasRect.top;
+                if (this.isClickingOnElement(clickX, clickY, obj)) {
+                    console.log('mute clicked')
+                }
+            });
+        }
+    }
+
+    startFullscreenListener(obj) {
+        if (obj instanceof Option && obj.isFullscreenSwitch) {
+            this.canvas.addEventListener('click', (event) => {
+                const canvasRect = this.canvas.getBoundingClientRect();
+                const clickX = event.clientX - canvasRect.left;
+                const clickY = event.clientY - canvasRect.top;
+                if (this.isClickingOnElement(clickX, clickY, obj)) {
+                    console.log('fulls klicked')
+                    this.canvas.requestFullscreen()
+                }
+            });
+        }
+    }
+
+    isClickingOnElement(clickX, clickY, obj) {
+        return clickX >= obj.positionX &&
+            clickX <= obj.positionX + obj.width &&
+            clickY >= obj.positionY &&
+            clickY <= obj.positionY + obj.height;
     }
 
     draw(ctx) {
